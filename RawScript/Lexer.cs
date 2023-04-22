37e9f34be062e68d548116f7014312f2530a2d64
redeboypro <redeboypro@gmail.com>
2023-04-22 15:29:32 +0300

    Small fixes

--- RawScript/Lexer.cs
+++ RawScript/Lexer.cs
@@ -15,6 +15,10 @@ namespace RawScript
                 case '%' :
                 case '+' :
                 case '-' :
+                case '>' :
+                case '<' :
+                case '|' :
+                case '&' :
                 case '(' :
                 case ')' :
                 case '{' :
--- RawScriptDemo/bin/Debug/test.rs
+++ RawScriptDemo/bin/Debug/test.rs
@@ -1,8 +1,2 @@
 var i;
-let i = 0;
-
-while i < 10
-{
-    print i;
-    let i + 1;
-}
+let i = 0;while i<10{print i;let i+1;}
